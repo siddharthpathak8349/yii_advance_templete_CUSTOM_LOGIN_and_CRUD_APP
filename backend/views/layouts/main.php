a8035b1cef245e0e078e31546d950a4f439a8af5
siddharth yt 1 <siddworldyt@gmail.com>
2024-12-04 18:03:11 +0530

    changes

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -36,6 +36,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Book', 'url' => ['/book/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
